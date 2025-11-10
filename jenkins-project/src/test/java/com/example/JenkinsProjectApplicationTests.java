1a70bde03dc7a54903db9c4051fee81c40b0c5f0
harsha-demo <vivekchinchole98@gmail.com>
2025-11-10 22:41:27 +0530

    remove logger statement

--- jenkins-project/src/test/java/com/example/JenkinsProjectApplicationTests.java
+++ jenkins-project/src/test/java/com/example/JenkinsProjectApplicationTests.java
@@ -15,7 +15,6 @@ class JenkinsProjectApplicationTests {
 	@Test
 	public void contextLoads() {
 		logger.info("Test case executing...");
-		logger.info("Test case executing second log statement ...");
 		assertEquals(true, true);	}
 
 }
